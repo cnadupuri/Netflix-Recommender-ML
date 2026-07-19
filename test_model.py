7767c94d595f1b89a4f1fe47b22bbca1165ea9e2
chaitanya <cnadupuri@gmail.com>
2026-07-19 16:59:11 +0530

    Enhance file existence checks with messages
    Updated assertions to include error messages and modified print statement.

--- test_model.py
+++ test_model.py
@@ -1,8 +1,6 @@
 import os
 
-assert os.path.exists("model(netflix).pkl")
-assert os.path.exists("preprocessor(netflix).pkl")
+assert os.path.exists("model(netflix).pkl"), "Model file missing!"
+assert os.path.exists("preprocessor(netflix).pkl"), "Preprocessor file missing!"
 
-print("Model exists")
-print("Preprocessor exists")
-print("Everything is OK")
+print("All files found.")
