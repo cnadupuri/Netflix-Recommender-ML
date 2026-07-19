4339078373ab756ae37b0653dd6cc77b40a6314c
chaitanya <cnadupuri@gmail.com>
2026-07-19 16:49:16 +0530

    Add tests for model and preprocessor file existence

---
+++ test_model.py
@@ -0,0 +1,8 @@
+import os
+
+assert os.path.exists("model(netflix).pkl")
+assert os.path.exists("preprocessor(netflix).pkl")
+
+print("Model exists")
+print("Preprocessor exists")
+print("Everything is OK")
